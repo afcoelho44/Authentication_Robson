5a3444fb0bf1dee28538373064dc1d8e70592d1a
Ana Fábia Coelho <afcoelho44@gmail.com>
2025-06-15 16:14:01 -0300

    Commit inicial

---
+++ src/main/java/udesc/br/Authentication/Robson/AuthenticationRobsonApplication.java
@@ -0,0 +1,13 @@
+package udesc.br.Authentication.Robson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationRobsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationRobsonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/udesc/br/Authentication/Robson/AuthenticationRobsonApplicationTests.java
@@ -0,0 +1,13 @@
+package udesc.br.Authentication.Robson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationRobsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
